2c97f0735990d93c5d255d1521002265dc71dfa9
geekyuttu <utkarsh13ranjan@gmail.com>
2021-07-08 13:00:02 +0530

    debugged code

--- pacman-dnn.py
+++ pacman-dnn.py
@@ -1,7 +1,8 @@
 import numpy as np
 import gym
 import matplotlib.pyplot as plt
-from numpy.core.numeric import Inf
+from math import inf
+import os
 
 class Target_network:
 
@@ -27,11 +28,14 @@ class Target_network:
 
     def linear_forward(self,A, W, b):
 
+        # print('A shape in linear forward', A.shape)
+        # print("W shape : ", W.shape)
         Z = np.dot(W, A) + b
+        # print("b shape : ", b.shape)
+        # print("z shape: " , Z.shape)
         cache = (A, W, b)
         
         return Z, cache
-
 
     def linear_activation_forward(self, A_prev, W, b, activation):
         
@@ -43,7 +47,6 @@ class Target_network:
             Z, linear_cache = self.linear_forward(A_prev, W, b)
             A, activation_cache = self.relu(Z)
         
-        assert (A.shape == (W.shape[0], A_prev.shape[1]))
         cache = (linear_cache, activation_cache)
 
         return A, cache
@@ -53,21 +56,17 @@ class Target_network:
         caches = []
         A = X
         L = len(parameters) // 2                  # number of layers in the neural network
-        
+
+        # print(L)
         # Implement [LINEAR -> RELU]*(L-1). Add "cache" to the "caches" list.
         for l in range(1, L):
+            # print(A.shape)
             A_prev = A 
-            A, cache = self.linear_activation_forward(A_prev, 
-                                                parameters['W' + str(l)], 
-                                                parameters['b' + str(l)], 
-                                                activation='relu')
+            A, cache = self.linear_activation_forward(A_prev, parameters['W' + str(l)], parameters['b' + str(l)], activation='relu')
             caches.append(cache)    
         
         # Implement LINEAR -> SIGMOID. Add "cache" to the "caches" list.
-        AL, cache = self.linear_activation_forward(A, 
-                                                parameters['W' + str(L)], 
-                                                parameters['b' + str(L)], 
-                                                activation='sigmoid')
+        AL, cache = self.linear_activation_forward(A, parameters['W' + str(L)], parameters['b' + str(L)], activation='sigmoid')
         caches.append(cache)
 
         return AL, caches
@@ -92,22 +91,13 @@ class Prediction_network(Target_network):
         # When z <= 0, we should set dz to 0 as well. 
         dZ[Z <= 0] = 0
         return dZ
-    
-    def linear_backward(self, dZ, cache):
-
-        A_prev, W, b = cache
-        m = A_prev.shape[1]
-
-        dW = np.dot(dZ, cache[0].T) / m
-        db = np.squeeze(np.sum(dZ, axis=1, keepdims=True)) / m
-        dA_prev = np.dot(cache[1].T, dZ)
-
-        return dA_prev, dW, db
 
     def compute_cost(self, AL, Y):
-    
-        m = Y.shape[1]
-        cost = (-1 / m) * np.sum(np.multiply(Y, np.log(AL)) + np.multiply(1 - Y, np.log(1 - AL)))
+        
+        # print(Y)
+        Y = np.ones((AL.shape[0],1)) * Y
+        # print(Y)
+        cost = -np.sum(np.multiply(Y, np.log(AL)) + np.multiply(1 - Y, np.log(1 - AL)))
         cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
         
         return cost
@@ -115,21 +105,20 @@ class Prediction_network(Target_network):
     def linear_backward(self, dZ, cache):
 
         A_prev, W, b = cache
-        m = A_prev.shape[1]
 
-        dW = np.dot(dZ, cache[0].T) / m
-        db = np.squeeze(np.sum(dZ, axis=1, keepdims=True)) / m
+        # print("dZ shape",dZ.shape)
+        dW = np.dot(dZ, cache[0].T)
+        db = dZ
         dA_prev = np.dot(cache[1].T, dZ)
         
         return dA_prev, dW, db
 
     def linear_activation_backward(self, dA, cache, activation):
 
-        linear_cache, activation_cache = cache
+        linear_cache, activation_cache = cache   
         
         if activation == "relu":
             dZ = self.relu_backward(dA, activation_cache)
-            
         elif activation == "sigmoid":
             dZ = self.sigmoid_backward(dA, activation_cache)
         
@@ -141,21 +130,21 @@ class Prediction_network(Target_network):
 
         grads = {}
         L = len(caches) # the number of layers
-        m = AL.shape[1]
-        Y = Y.reshape(AL.shape) # after this line, Y is the same shape as AL
-        
-        dAL = dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+        # m = AL.shape[0]
+        Y = np.ones((AL.shape[0],1)) * Y
+        # Y = Y.reshape(AL.shape) # after this line, Y is the same shape as AL
+        # print(L)
+        dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
 
         current_cache = caches[-1]
-        grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = self.linear_backward(self.sigmoid_backward(dAL, current_cache[1]),current_cache[0])
-
+        grads["dA" + str(L-1)], grads["dW" + str(L)], grads["db" + str(L)] = self.linear_activation_backward(dAL, current_cache, "sigmoid")
         
         for l in reversed(range(L-1)):
             # lth layer: (RELU -> LINEAR) gradients.
             # Inputs: "grads["dA" + str(l + 2)], caches". Outputs: "grads["dA" + str(l + 1)] , grads["dW" + str(l + 1)] , grads["db" + str(l + 1)] 
             current_cache = caches[l]
-            dA_prev_temp, dW_temp, db_temp = self.linear_backward(self.sigmoid_backward(dAL, caches[1]), caches[0])
-            grads["dA" + str(l + 1)] = dA_prev_temp
+            dA_prev_temp, dW_temp, db_temp = self.linear_activation_backward(grads["dA" + str(l+1)], current_cache, "relu")
+            grads["dA" + str(l)] = dA_prev_temp
             grads["dW" + str(l + 1)] = dW_temp
             grads["db" + str(l + 1)] = db_temp
 
@@ -174,14 +163,14 @@ class Prediction_network(Target_network):
 
 def epsilon_greedy_action(Q,eps):
     if np.random.random() < eps: # explore
-        return np.random.randint(Q.size())
+        # print(type(Q))
+        return np.random.randint(9)
     else: # exploit
         return np.random.choice(np.flatnonzero(Q == Q.max()))
 
-
 env = gym.make('MsPacman-v0')
 observation = env.reset()
-state = observation.reshape(-1).T
+state = observation.reshape(-1,1)
 
 target_network = Target_network()
 prediction_network = Prediction_network()
@@ -190,24 +179,30 @@ gamma = 0.8
 c = 100 # not sure
 eps = 0.5
 
-layer_dims = (observation.size,7,env.action_space.n)
+layer_dims = (observation.size,30,20,10,env.action_space.n)
 parameters = prediction_network.initialize_parameters_deep(layer_dims)
 target_parameters = parameters
 
 N_episode = 10000
 for i_episode in range(N_episode):
-    for t in range(Inf):
+    t = 0
+    while(True):
+        # print(t)
         env.render()
         if(i_episode % c == 0): target_parameters = parameters
+        # print(state.shape)
         Q, caches = prediction_network.L_model_forward(state, parameters)
+        # print(Q)
         action = epsilon_greedy_action(Q,eps)
         observation , reward , done , _ = env.step(action)
-        state = observation.reshape(-1).T
-        next_Q, caches = target_network.L_model_forward(state, target_parameters)
+        state = observation.reshape(-1,1)
+        next_Q, _ = target_network.L_model_forward(state, target_parameters)
         target = reward + gamma * next_Q.max()
         cost = prediction_network.compute_cost(Q, target)
         grads =  prediction_network.L_model_backward(Q, target, caches)
         parameters = prediction_network.update_parameters(parameters, grads, learning_rate)
+        # print(parameters)
+        t+=1
 
         if done:
             print("Episode finished after {} timesteps".format(t+1))
